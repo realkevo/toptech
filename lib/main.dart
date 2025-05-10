d7c5348b8ba7ba9da9f4f60337bbf772d68fef8b
realkevo <kelvinthuranira591@gmail.com>
2025-05-10 22:44:31 +0300

    cleaned code

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:toptech/stateTv/desktophomepagedisplay.dart';
-//backupCode
+
+//backupCode cleaned
 // Replace this with your actual Firebase options class
 import 'firebase_options.dart';
 
