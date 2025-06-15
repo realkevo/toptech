429356f87ab49af8316f531fb824efcc617fce09
KelvinThuranira <58592608+realkevo@users.noreply.github.com>
2025-06-15 18:06:53 +0300

    Update remarkDisplay.dart
    remark Timestamp display fixed

--- lib/stateTv/remarkDisplay.dart
+++ lib/stateTv/remarkDisplay.dart
@@ -148,18 +148,16 @@ class _RemarkDisplayClassState extends State<RemarkDisplayClass> {
   }
 }
 */
-import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class RemarkDisplayClass extends StatefulWidget {
   @override
-  _RemarkDisplayClassState createState() =>
-      _RemarkDisplayClassState();
+  _RemarkDisplayClassState createState() => _RemarkDisplayClassState();
 }
 
 class _RemarkDisplayClassState extends State<RemarkDisplayClass> {
-  List<DocumentSnapshot> _remarks = []; 
+  List<DocumentSnapshot> _remarks = [];
   ScrollController _scrollController = ScrollController();
   bool _isPointerDown = false;
 
@@ -172,7 +170,6 @@ class _RemarkDisplayClassState extends State<RemarkDisplayClass> {
   void _startAutoplay() async {
     while (mounted) {
       await Future.delayed(Duration(seconds: 5));
-
       if (!_isPointerDown && _remarks.isNotEmpty) {
         _scrollToNextRemark();
       }
@@ -213,105 +210,86 @@ class _RemarkDisplayClassState extends State<RemarkDisplayClass> {
           ),
           StreamBuilder<QuerySnapshot>(
             stream:
-                FirebaseFirestore.instance.collection('remarkData').snapshots(), 
+                FirebaseFirestore.instance.collection('remarkData').snapshots(),
             builder: (context, snapshot) {
-              if (!snapshot.hasData ||
-                 snapshot.data!.docs.isEmpty) {
+              if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
                 return Center(child: Text('No remarks found.'));
               }
 
               _remarks = snapshot.data!.docs;
-
-              List<DocumentSnapshot> duplicatedRemarks = [
-                ..._remarks,
-                ..._remarks
-              ];
+              List<DocumentSnapshot> duplicatedRemarks = [..._remarks, ..._remarks];
 
               return Listener(
                 onPointerDown: (_) {
-                    setState(() {
-                      _isPointerDown = true;
-                    });
+                  setState(() {
+                    _isPointerDown = true;
+                  });
                 },
                 onPointerUp: (_) {
-                    setState(() {
-                      _isPointerDown = false;
-                    });
+                  setState(() {
+                    _isPointerDown = false;
+                  });
                 },
                 child: SingleChildScrollView(
-                    controller: _scrollController,
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children:
-                          duplicatedRemarks.map((currentRemark) {
-                        var name = currentRemark['remarkName'];
-                        var description = currentRemark['remarkDescription'];
+                  controller: _scrollController,
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: duplicatedRemarks.map((currentRemark) {
+                      var name = currentRemark['remarkName'] ?? '';
+                      var description = currentRemark['remarkDescription'] ?? '';
+                      var date = currentRemark['remarkDate'] ?? '';
 
-                        return Container(
-                          margin:
-                                const EdgeInsets.symmetric(horizontal: 6),
-                          width: 250,
-                          height: 180,
-                          decoration: BoxDecoration(
-                            color: Colors.transparent,
-                            border: Border.all(
-                                color: Colors.lightGreenAccent,
-                                width: 2),
-                            borderRadius: BorderRadius.circular(12),
-                            gradient: LinearGradient(
-                                begin: Alignment.topLeft,
-                                end: Alignment.bottomRight,
-                                colors: [
-                                  Color(0xFF0A0E21),
-                                  Color(0xFF1E3C72),
-                                ],
-                             ),
-                           ),
-                           padding: const EdgeInsets.all(5),
-                           child: Column(
-                             mainAxisAlignment: MainAxisAlignment.center,
-                             children: [
-                                Text(
-                                  name,
-                                  style: TextStyle(
-                                      color: Colors.orange,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                SizedBox(height: 4),
-                                Text(
-                                  description,
-                                  style: TextStyle(color: Colors.white),
-                                  textAlign: TextAlign.center,
-                                ),
-                                SizedBox(height: 4),
-                                Text(
-                                  (currentRemark['timestamp']
-                                      as Timestamp)
-                                      .toDate()
-                                      .day
-                                      .toString()
-                                  + '/'
-                                  + (currentRemark['timestamp']
-                                      as Timestamp)
-                                      .toDate()
-                                      .month
-                                      .toString()
-                                  + '/'
-                                  + (currentRemark['timestamp']
-                                      as Timestamp)
-                                      .toDate()
-                                      .year
-                                      .toString(), 
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontStyle: FontStyle.italic),
-                                ),
-                             ],
-                           ),
-                         );
-                      }).toList(), 
-                    ),
-                 ),
+                      return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 6),
+                        width: 250,
+                        height: 180,
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          border: Border.all(
+                            color: Colors.lightGreenAccent,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(12),
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              Color(0xFF0A0E21),
+                              Color(0xFF1E3C72),
+                            ],
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(5),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              name,
+                              style: TextStyle(
+                                color: Colors.orange,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              description,
+                              style: TextStyle(color: Colors.white),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              date,  // Display the stored date string here
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontStyle: FontStyle.italic,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
               );
             },
           ),
